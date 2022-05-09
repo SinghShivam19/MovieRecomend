d579453a1ab295279d99c4ae7d22c7db45bf3f02
SinghShivam19 <89136395+SinghShivam19@users.noreply.github.com>
2022-05-10 01:07:17 +0530

    Update movie3.py

--- movie3.py
+++ movie3.py
@@ -3,8 +3,8 @@ import pickle
 import pandas as pd
 import requests
 st.title('Movie Recommende System')
-movie_list=pickle.load(open('movie_list.pkl','rb'))
-similarity=pickle.load(open('similarity.pkl','rb'))
+movie_list=pickle.load(open('model/movie_list.pkl','rb'))
+similarity=pickle.load(open('model/similarity.pkl','rb'))
 movies = pd.DataFrame(movie_list)
 
 def fetch_poster(movie_id):
